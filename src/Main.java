d2320a275822f8214b2a303f041b9d625c4678d5
lukeh <hensonlt@mail.uc.edu>
2024-09-23 23:09:44 -0400

    task 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner in = new Scanner(System.in);
+
+       double springCost;
+       double summerCost;
+       double autumnCost;
+       double winterCost;
+       double totalCost;
+
+        System.out.println("What was your Maintenance cost this spring?");
+        springCost = in.nextDouble();
+        System.out.println("What was your Maintenance cost this summer?");
+        summerCost = in.nextDouble();
+        System.out.println("What was your Maintenance cost this autumn?");
+        autumnCost = in.nextDouble();
+        System.out.println("What was your Maintenance cost this winter?");
+        winterCost = in.nextDouble();
+        totalCost = springCost + summerCost + autumnCost + winterCost;
+        System.out.println("The total cost is: " + totalCost + " Your spring cost was " + springCost + " Your summer cost was " + summerCost + " Your autumn cost was " + autumnCost + " Your winter cost was " + winterCost);
+
+
+
     }
 }
